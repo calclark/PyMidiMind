528eb9ba6343f565098ec6239cd19b514ed8259a
explosionsrgood <redhatnovember18@gmail.com>
2019-12-08 04:25:37 -0500

    Constructed MidiNote class

---
+++ midimind/midi_note.py
@@ -0,0 +1,26 @@
+import mido.messages.messages as msgs
+
+class MidiNote:
+
+    def __init__(self, on_msg: msgs.Message, off_msg: msgs.Message):
+        self.on_msg = on_msg
+        self.off_msg = off_msg
+
+    @property
+    def velocity(self):
+        return self.on_msg.velocity
+
+    @property
+    def channel(self):
+        return self.on_msg.channel
+    
+    @property
+    def note(self):
+        return self.on_msg.note
+    
+    @property
+    def duration(self):
+        return self.off_msg.time / (self.on_msg.time + self.off_msg.time)
+    
+    
+    
